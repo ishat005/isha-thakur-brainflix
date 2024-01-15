cf4892180fa5fef927e833e99bf175d67c5bdb06
isha <IshaGajendrasingh.Thakur@student.ufv.ca>
2024-01-15 15:16:00 -0800

    Modified stylings for videoUpload page

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -3,14 +3,52 @@ import { Link } from 'react-router-dom';
 import { useState} from 'react';
 import publishLogo from '../../assets/icons/publish.svg';
 import './VideoUpload.scss';
-
-
+import axios from 'axios';
+const API_URL = 'http://localhost:5001'
 
 function VideoUpload(){
     const [uploadSuccess, setUploadSuccess] = useState(false);
+    const [uploadTitle, setUploadTitle] = useState('');
+    const [uploadDescription, setUploadDescription] = useState('');
+
+    const videoUpload = async() => {
+        const videoData = {
+            title: uploadTitle,
+            description: uploadDescription
+        }
+        try{
+            let res = await axios.post(`${API_URL}/videos`, videoData);
+            return res;
+        } catch(err){
+            console.log("Error: ", err);
+        }
+    }
+
+    const handleTitleChange = (e) => {
+        if(e.target.value === ""){
+            alert("Please enter video title to be uploaded!");
+        } else{
+            setUploadTitle(e.target.value);
+        }
+    }
+
+    const handleDescriptionChange = (e) => {
+        if(e.target.value === ""){
+            alert("Please enter video description to be uploaded!");
+        } else{
+            setUploadDescription(e.target.value);
+        }
+    }
 
     const handleUpload = (event) => {
-        setUploadSuccess(true);
+        if(!handleTitleChange && !handleDescriptionChange){
+            alert("Details missing");
+            return false;
+        } else{
+            videoUpload();
+            setUploadSuccess(true);
+        }
+        return true;
     }
 
     return(
@@ -25,31 +63,32 @@ function VideoUpload(){
 
                     <div className="videoTitleContainer">
                         <label id="videoTitle"> TITLE YOUR VIDEO </label>
-                        <input id="videoTitleInput" placeholder="Add a title to your video"/>
+                        <input id="videoTitleInput" onChange={handleTitleChange} placeholder="Add a title to your video"/>
                     </div>
 
                     <div className="videoDescriptionContainer">
                         <label id="videoDescription"> ADD A VIDEO DESCRIPTION </label>
-                        <textarea type="text" id="addvideoDescription" name="addvideoDescription" rows="7" cols="50" placeholder='Add a description to your video'>
+                        <textarea type="text" id="addvideoDescription"  onChange={handleDescriptionChange}  name="addvideoDescription" rows="7" cols="50" placeholder='Add a description to your video' >
                         </textarea>
+                        
                     </div>
 
                     <div>
                      { 
                         uploadSuccess ? (
                             <>
-                                <div> Upload successful</div>
+                                <div className="uploadSuccess"> Upload successful</div>
                                 <div>
                                     <Link to={'/'}>
-                                        <button type="button">
-                                            Successful
+                                        <button type="button" className="home">
+                                            Home
                                         </button>
                                     </Link>
                                 </div> 
                             </>        
                         ) : (
                             <>
-                                <button type="submit" className="publishButton">
+                                <button type="submit" className="publishButton" onClick={handleUpload}>
                                     <img src={publishLogo} alt="A logo that is contained and displayed within a button" className="publish" />
                                         PUBLISH
                                 </button>
@@ -65,6 +104,7 @@ function VideoUpload(){
             </main>  
         </div>  
     )
+    
 }
 
 export default VideoUpload;
